02b187d5b1da8ea5fcd0275c9f811f69f5092860
Amisha <amishamallick21@gmail.com>
2024-04-03 17:24:02 +0530

    added new files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+// import axios from 'axios';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import "./App.css";
+import { AdminDashboardPage } from "./Routes/AdminRoutes.js";
+import ProtectedAdminRoute from "./Routes/ProtectedAdminRoute.js";
+import { AboutUsPage, ActivationPage, BuyerPolicyPage, ContactUsPage, FAQPage, HomePage, LoginPage, PrivacyPolicyPage, ProductsPage, SellerPolicyPage, ShopCreatePage, ShopLoginPage, SignUpPage, TermsandConditionPage } from "./Routes/Routes.js";
 
-function App() {
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/sign-up' element={<SignUpPage />} />
+        <Route path='/activation/:activation_token' element={<ActivationPage />} />
+        <Route path='/' element={<HomePage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/FAQs" element={<FAQPage />} />
+        <Route path="/TermsandConditions" element={<TermsandConditionPage />} />
+        <Route path="/ContactUs" element={<ContactUsPage />} />
+        <Route path="/shop-create" element={<ShopCreatePage />} />
+        <Route path="/shop-login" element={<ShopLoginPage />} />
+        <Route path="/PrivacyPolicy" element={<PrivacyPolicyPage />} />
+        <Route path="/AboutUs" element={<AboutUsPage />} />
+        <Route path="/SellerPolicy" element={<SellerPolicyPage />} />
+        <Route path="/BuyerPolicy" element={<BuyerPolicyPage />} />
+        <Route path="/admin/dashboard" element={ <ProtectedAdminRoute> <AdminDashboardPage /> </ProtectedAdminRoute> }/>
+      </Routes>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
